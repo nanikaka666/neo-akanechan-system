4db027373ef1ee5e69fc91cc6440bf0a714a2f3f
nanikaka <nanikaka666@gmail.com>
2025-10-11 01:24:49 +0900

    implements IpcRendererWrapper

---
+++ src/ipcRendererWrapper.ts
@@ -0,0 +1,28 @@
+import { ipcRenderer } from "electron";
+import { IpcEvent } from "./ipcEvent";
+
+/**
+ * Wrapper object of ipcRenderer.
+ *
+ * functions in ipcRenderer accept arbitrary combinations of key and value.
+ * this wrapper has typed parameters to prevent passing unexpected pair.
+ */
+export const IpcRendererWrapper = {
+  invoke: <K extends keyof IpcEvent>(
+    key: K,
+    ...args: Parameters<IpcEvent[K]>
+  ): Promise<ReturnType<IpcEvent[K]>> => {
+    return ipcRenderer.invoke(key, ...args);
+  },
+};
+
+declare global {
+  namespace Electron {
+    interface IpcRenderer extends NodeJS.EventEmitter {
+      /**
+       * @deprecated use IpcRendererWrapper.invoke() instead.
+       */
+      invoke(channel: string, ...args: any[]): Promise<any>;
+    }
+  }
+}
