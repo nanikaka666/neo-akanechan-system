69a88d753b7d56017ab736d7ac13ae6736c2a232
nanikaka <nanikaka666@gmail.com>
2025-10-11 01:59:32 +0900

    definition IpcApi for renderer process

---
+++ src/preload/ipcApi.ts
@@ -0,0 +1,14 @@
+import { IpcEvent } from "../ipcEvent";
+
+type Invoke<K extends keyof IpcEvent> = (
+  ...args: Parameters<IpcEvent[K]>
+) => Promise<ReturnType<IpcEvent[K]>>;
+
+/**
+ * Api interface exposed to Renderer process.
+ */
+export interface IpcApi {
+  ipcApi: {
+    requestChannelTitleMatchTo: Invoke<"confirmInputChannelId">;
+  };
+}
