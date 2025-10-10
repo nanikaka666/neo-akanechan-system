77c60afcc27b2220f1a6bc59b0a9523e6d59712e
nanikaka <nanikaka666@gmail.com>
2025-10-11 00:55:44 +0900

    put IpcEvent file

---
+++ src/ipcEvent.ts
@@ -0,0 +1,10 @@
+import { ChannelId, ChannelTitle } from "youtube-live-scraper";
+
+/**
+ * Ipc process interfaces.
+ *
+ * key represents channel name of ipc process.
+ */
+export interface IpcEvent {
+  confirmInputChannelId: (inputChannelId: ChannelId) => ChannelTitle;
+}
