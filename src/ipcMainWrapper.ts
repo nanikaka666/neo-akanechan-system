8ce293981e88e27adebc7d861471e51b0852a404
nanikaka <nanikaka666@gmail.com>
2025-10-11 01:47:10 +0900

    implements ipcMainWrapper

---
+++ src/ipcMainWrapper.ts
@@ -0,0 +1,34 @@
+import { ipcMain, IpcMainInvokeEvent } from "electron";
+import { IpcEvent } from "./ipcEvent";
+
+/**
+ * Wrapper object of ipcMain.
+ *
+ * functions in ipcMain accept arbitrary combinations of key and value.
+ * this wrapper has typed parameters to prevent passing unexpected pair.
+ */
+export const IpcMainWrapper = {
+  handle: <K extends keyof IpcEvent>(
+    key: K,
+    callback: (
+      e: IpcMainInvokeEvent,
+      ...args: Parameters<IpcEvent[K]>
+    ) => Promise<ReturnType<IpcEvent[K]>>,
+  ) => {
+    ipcMain.handle(key, callback);
+  },
+};
+
+declare global {
+  namespace Electron {
+    interface IpcMain extends NodeJS.EventEmitter {
+      /**
+       * @deprecated use IpcMainWrapper.handle() instead.
+       */
+      handle(
+        channel: string,
+        listener: (event: IpcMainInvokeEvent, ...args: any[]) => Promise<any> | any,
+      ): void;
+    }
+  }
+}
