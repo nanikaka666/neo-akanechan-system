a9bc966f0a8c485db84be22824ea6216ea228b7e
nanikaka <nanikaka666@gmail.com>
2025-10-13 06:03:44 +0900

    chore: add docs to IpcEvent

--- src/ipcEvent.ts
+++ src/ipcEvent.ts
@@ -6,5 +6,11 @@ import { ChannelId, ChannelTitle } from "youtube-live-scraper";
  * key represents channel name of ipc process.
  */
 export interface IpcEvent {
+  /**
+   * this event will be fired when need to check channel id.
+   * "check" means confirm of existing or fetch data about the channel.
+   *
+   * if the channel was not found match to `inputChannelId`, `undefined` will be returned.
+   */
   confirmInputChannelId: (inputChannelId: ChannelId) => ChannelTitle | undefined;
 }
