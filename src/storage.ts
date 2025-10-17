4e144f15da2a729b91cce007fda146604b097252
nanikaka <nanikaka666@gmail.com>
2025-10-18 07:34:30 +0900

    add StorageService

--- src/storage.ts
+++ src/storage.ts
@@ -1,4 +1,5 @@
 import Store from "electron-store";
+import { ChannelId } from "youtube-live-scraper";
 
 interface StorageData {
   mainChannelId: string;
@@ -18,5 +19,33 @@ export const Storage = {
   },
   set: <K extends keyof StorageData>(key: K, value: StorageData[K]) => {
     store.set(key, value);
+  },
+  delete: <K extends keyof StorageData>(key: K) => {
+    store.delete(key);
+  },
+};
+
+/**
+ * This object has functions contains application logics.
+ */
+export const StorageService = {
+  /**
+   * Get the main ChannelId.
+   *
+   * if main channel was not there, `undefined` will be returned.
+   */
+  getMainChannelId: () => {
+    const mainChannelId = Storage.get("mainChannelId");
+    if (mainChannelId === undefined) {
+      return undefined;
+    } else {
+      try {
+        return new ChannelId(mainChannelId);
+      } catch {
+        // If process will be reached in here, stored data is broken. Delete it.
+        Storage.delete("mainChannelId");
+        return undefined;
+      }
+    }
   },
 };
