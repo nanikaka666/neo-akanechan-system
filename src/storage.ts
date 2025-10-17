1e608d571b7626a6e0d2b482fd94b05b4c51f090
nanikaka <nanikaka666@gmail.com>
2025-10-18 07:21:28 +0900

    chore: add docs to Storage object

--- src/storage.ts
+++ src/storage.ts
@@ -1,11 +1,17 @@
 import Store from "electron-store";
 
-export interface StorageData {
+interface StorageData {
   mainChannelId: string;
 }
 
 const store = new Store<StorageData>();
 
+/**
+ * This object used to access the storage.
+ *
+ * Detail of storage is hidden to a caller.
+ * So, storage implementations will be changed with no effect for a caller.
+ */
 export const Storage = {
   get: <K extends keyof StorageData>(key: K): StorageData[K] => {
     return store.get(key);
