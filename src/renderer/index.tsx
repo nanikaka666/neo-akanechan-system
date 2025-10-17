6db8abbcafd3f5b7cda131156b71bad2c87b749a
nanikaka <nanikaka666@gmail.com>
2025-10-18 06:10:42 +0900

    use App components

--- src/renderer/index.tsx
+++ src/renderer/index.tsx
@@ -1,5 +1,5 @@
-import { MainWindow } from "./components/MainWindow";
+import { App } from "./components/App";
 import "./index.css";
 import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById("root")!).render(<MainWindow></MainWindow>);
+createRoot(document.getElementById("root")!).render(<App></App>);
