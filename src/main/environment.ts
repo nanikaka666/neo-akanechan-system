c1c8c5cacd637750d2a5dbfbc1975a32315f1cc0
nanikaka <nanikaka666@gmail.com>
2025-10-19 08:48:31 +0900

    add platform() to fetch environment

--- src/main/environment.ts
+++ src/main/environment.ts
@@ -3,3 +3,13 @@ import { app } from "electron";
 export function isDevMode() {
   return app.isPackaged;
 }
+
+export function platform() {
+  if (process.platform === "darwin") {
+    return "mac";
+  }
+  if (process.platform === "win32") {
+    return "windows";
+  }
+  throw new Error("this app runs on only 'Windows' or 'MacOS'.");
+}
