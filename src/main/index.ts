6493f5b3ee14ba9c50bfe84dc0bde042e055b452
nanikaka <nanikaka666@gmail.com>
2025-10-21 09:10:58 +0900

    refactor: extract to ipcMainHandlers file

--- src/main/index.ts
+++ src/main/index.ts
@@ -1,12 +1,9 @@
 import { app, BrowserWindow } from "electron";
 import { checkElectronSquirrelStartup } from "./checkElectronSquirrelStartup";
-import { IpcMainWrapper } from "./ipcMainWrapper";
-import { PageFetcher, Scraper } from "youtube-live-scraper";
-import { StorageService } from "./storage";
 import { createWindow } from "./mainWindow";
 import { setupApplicationMenu } from "./menu";
 import { platform } from "./environment";
-import { WebContentsWrapper } from "./webContentsWrapper";
+import { setupIpcMainHandlers } from "./ipcMainHandlers";
 
 /**
  * Quit when all windows are closed, except on macOS. There, it's common
@@ -42,64 +39,7 @@ function main() {
 
   app.on("window-all-closed", onWindowAllClosed);
 
-  IpcMainWrapper.handle("confirmInputChannelId", async (e, inputChannelId) => {
-    try {
-      const page = await PageFetcher.getChannelPage(inputChannelId);
-      return {
-        channelId: inputChannelId, //TODO: convert to Youtube ID style if needed.
-        channelTitle: Scraper.getChannelTitleFromChannelPage(page),
-        subscribersCount: Scraper.getSubscriberCountFromChannelPage(page),
-        ownerIcon: Scraper.getOwnerIconUrlFromChannelPage(page),
-      };
-    } catch {
-      return undefined;
-    }
-  });
-
-  IpcMainWrapper.handle("getMainChannelId", () => {
-    return Promise.resolve(StorageService.getMainChannelId());
-  });
-
-  IpcMainWrapper.handle("registerChannel", (e, channelId) => {
-    if (channelId.isHandle) {
-      return Promise.resolve(false);
-    }
-    if (!StorageService.registerChannelIdAndMarkAsMain(channelId)) {
-      return Promise.resolve(false);
-    }
-    WebContentsWrapper.send(e.sender, "tellNewMainChannelId", channelId);
-    return Promise.resolve(true);
-  });
-
-  IpcMainWrapper.handle("getChannelTop", async (e, channelId) => {
-    try {
-      const channelPage = await PageFetcher.getChannelPage(channelId);
-      const channel = {
-        channelId: channelId,
-        channelTitle: Scraper.getChannelTitleFromChannelPage(channelPage),
-        subscribersCount: Scraper.getSubscriberCountFromChannelPage(channelPage),
-        ownerIcon: Scraper.getOwnerIconUrlFromChannelPage(channelPage),
-        channelBanner: Scraper.getChannelBanner(channelPage),
-      };
-
-      const maybeClosestLivePage = await PageFetcher.getLivePage(channelId);
-
-      const closestLive =
-        maybeClosestLivePage.type === "channel"
-          ? undefined
-          : {
-              title: Scraper.getVideoTitle(maybeClosestLivePage),
-              // thumbnail: Scraper.getVideoThumbnail(maybeClosestLivePage),
-              thumbnail: `https://i.ytimg.com/vi/${maybeClosestLivePage.videoId.id}/maxresdefault.jpg`, // temporary aid
-              // isOnAir: Scraper.isLiveNow(maybeClosestLivePage),
-              isOnAir: !maybeClosestLivePage.html.includes(`"scheduledStartTime"`), // temporary aid
-            };
-      return { channel, closestLive };
-    } catch (e: unknown) {
-      console.log(e);
-      return undefined;
-    }
-  });
+  setupIpcMainHandlers();
 
   app.whenReady().then(() => {
     setupApplicationMenu();
---
+++ src/main/ipcMainHandlers.ts
@@ -0,0 +1,65 @@
+import { PageFetcher, Scraper } from "youtube-live-scraper";
+import { IpcMainWrapper } from "./ipcMainWrapper";
+import { StorageService } from "./storage";
+import { WebContentsWrapper } from "./webContentsWrapper";
+
+export function setupIpcMainHandlers() {
+  IpcMainWrapper.handle("confirmInputChannelId", async (e, inputChannelId) => {
+    try {
+      const page = await PageFetcher.getChannelPage(inputChannelId);
+      return {
+        channelId: inputChannelId, //TODO: convert to Youtube ID style if needed.
+        channelTitle: Scraper.getChannelTitleFromChannelPage(page),
+        subscribersCount: Scraper.getSubscriberCountFromChannelPage(page),
+        ownerIcon: Scraper.getOwnerIconUrlFromChannelPage(page),
+      };
+    } catch {
+      return undefined;
+    }
+  });
+
+  IpcMainWrapper.handle("getMainChannelId", () => {
+    return Promise.resolve(StorageService.getMainChannelId());
+  });
+
+  IpcMainWrapper.handle("registerChannel", (e, channelId) => {
+    if (channelId.isHandle) {
+      return Promise.resolve(false);
+    }
+    if (!StorageService.registerChannelIdAndMarkAsMain(channelId)) {
+      return Promise.resolve(false);
+    }
+    WebContentsWrapper.send(e.sender, "tellNewMainChannelId", channelId);
+    return Promise.resolve(true);
+  });
+
+  IpcMainWrapper.handle("getChannelTop", async (e, channelId) => {
+    try {
+      const channelPage = await PageFetcher.getChannelPage(channelId);
+      const channel = {
+        channelId: channelId,
+        channelTitle: Scraper.getChannelTitleFromChannelPage(channelPage),
+        subscribersCount: Scraper.getSubscriberCountFromChannelPage(channelPage),
+        ownerIcon: Scraper.getOwnerIconUrlFromChannelPage(channelPage),
+        channelBanner: Scraper.getChannelBanner(channelPage),
+      };
+
+      const maybeClosestLivePage = await PageFetcher.getLivePage(channelId);
+
+      const closestLive =
+        maybeClosestLivePage.type === "channel"
+          ? undefined
+          : {
+              title: Scraper.getVideoTitle(maybeClosestLivePage),
+              // thumbnail: Scraper.getVideoThumbnail(maybeClosestLivePage),
+              thumbnail: `https://i.ytimg.com/vi/${maybeClosestLivePage.videoId.id}/maxresdefault.jpg`, // temporary aid
+              // isOnAir: Scraper.isLiveNow(maybeClosestLivePage),
+              isOnAir: !maybeClosestLivePage.html.includes(`"scheduledStartTime"`), // temporary aid
+            };
+      return { channel, closestLive };
+    } catch (e: unknown) {
+      console.log(e);
+      return undefined;
+    }
+  });
+}
