38f764af5cd5c1a3d68c7c7aac1ff09de5b4dc3a
nanikaka <nanikaka666@gmail.com>
2025-10-22 10:41:03 +0900

    implements createOverlayWindow

---
+++ src/main/overlayWindow.ts
@@ -0,0 +1,18 @@
+import { BrowserWindow } from "electron";
+
+declare const OVERLAY_WEBPACK_ENTRY: string;
+declare const OVERLAY_PRELOAD_WEBPACK_ENTRY: string;
+
+export const createOverlayWindow = (): void => {
+  const overlayWindow = new BrowserWindow({
+    height: 600,
+    width: 800,
+    webPreferences: {
+      preload: OVERLAY_PRELOAD_WEBPACK_ENTRY,
+    },
+  });
+
+  overlayWindow.loadURL(OVERLAY_WEBPACK_ENTRY);
+
+  // mainWindow.webContents.openDevTools();
+};
