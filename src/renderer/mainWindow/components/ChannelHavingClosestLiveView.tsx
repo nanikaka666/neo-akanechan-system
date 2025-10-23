00fefd7ecbdb111e212da9b39a843f5b92abeb1f
nanikaka <nanikaka666@gmail.com>
2025-10-23 21:04:06 +0900

    refactor: remove ClosestLiveView component

--- src/renderer/mainWindow/components/ChannelHavingClosestLiveView.tsx
+++ src/renderer/mainWindow/components/ChannelHavingClosestLiveView.tsx
@@ -1,12 +1,35 @@
+import { useState, MouseEvent } from "react";
 import { ChannelHavingClosestLive } from "../../../ipcEvent";
 import { ChannelSummaryView } from "./ChannelSummaryView";
-import { ClosestLiveView } from "./ClosestLiveView";
 
 export function ChannelHavingClosestLiveView({ channel }: { channel: ChannelHavingClosestLive }) {
+  const [isConfirming, setIsConfirming] = useState(false);
+
+  async function onClick(e: MouseEvent<HTMLButtonElement>) {
+    e.preventDefault();
+    setIsConfirming((_) => true);
+
+    const res = await window.ipcApi.requestOpenOverlay(channel.closestLive);
+    console.log(res);
+    setIsConfirming((_) => false);
+  }
+
   return (
     <div>
       <ChannelSummaryView channelSummary={channel.channel} />
-      <ClosestLiveView closestLive={channel.closestLive} />
+      <div>
+        <p>Next Live</p>
+        <img
+          src={channel.closestLive.thumbnail}
+          alt="next live thumbnail"
+          style={{ width: "360px" }}
+        />
+        <div>{channel.closestLive.title.title}</div>
+        <div>{channel.closestLive.isOnAir ? "On Air" : "Prepareing"}</div>
+        <button onClick={onClick} disabled={isConfirming}>
+          Live Start
+        </button>
+      </div>
     </div>
   );
 }
