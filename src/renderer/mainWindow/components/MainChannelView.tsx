406f1dedbf21e3621ebd44fa45d1c71f586d5818
nanikaka <nanikaka666@gmail.com>
2025-10-23 20:46:51 +0900

    refactor: extract ChannelSummaryView component

--- src/renderer/mainWindow/components/MainChannelView.tsx
+++ src/renderer/mainWindow/components/MainChannelView.tsx
@@ -1,25 +1,11 @@
 import { ChannelTop } from "../../..//ipcEvent";
+import { ChannelSummaryView } from "./ChannelSummaryView";
 import { ClosestLiveView } from "./ClosestLiveView";
 
 export function MainChannelView({ channelTop }: { channelTop: ChannelTop }) {
   return (
     <div>
-      <div>
-        {channelTop.channel.channelBanner && (
-          <img
-            src={channelTop.channel.channelBanner}
-            alt="channel banner"
-            style={{ width: "100%" }}
-          />
-        )}
-        <img
-          src={channelTop.channel.ownerIcon}
-          alt="owner icon"
-          style={{ width: "64px", height: "64px" }}
-        />
-        <div>{channelTop.channel.channelTitle.title}</div>
-        <div>Subsriber: {channelTop.channel.subscribersCount}</div>
-      </div>
+      <ChannelSummaryView channelSummary={channelTop.channel} />
       {channelTop.type === "has_closest_live" ? (
         <ClosestLiveView closestLive={channelTop.closestLive} />
       ) : (
