9d55bb1015270ce420b4b449580b576c5af5a9a5
nanikaka <nanikaka666@gmail.com>
2025-11-01 03:37:50 +0900

    refactor: LiveControlPanel components place on MainApp

--- src/renderer/mainWindow/components/MainApp.tsx
+++ src/renderer/mainWindow/components/MainApp.tsx
@@ -2,9 +2,12 @@ import { useEffect, useState } from "react";
 import { ChannelRegistrationLoader } from "./channelRegistration/ChannelRegistrationLoader";
 import { ChannelId } from "youtube-live-scraper";
 import { MainChannelTopLoader } from "./top/MainChannelTopLoader";
+import { ChannelHavingClosestLive } from "../../../ipcEvent";
+import { LiveControlPanel } from "./liveControl/LiveControlPanel";
 
 export function MainApp() {
   const [mainChannelId, setMainChannelId] = useState<ChannelId>();
+  const [channel, setChannel] = useState<ChannelHavingClosestLive>();
 
   useEffect(() => {
     window.ipcApi.requestMainChannelId().then((ch) => {
@@ -12,11 +15,18 @@ export function MainApp() {
       window.ipcApi.registerNewMainChannelListener((e, channelId) => {
         setMainChannelId((_) => channelId);
       });
+      window.ipcApi.registerIsStartedOverlayListener((e, channelHavingClosestLive) => {
+        setChannel((_) => channelHavingClosestLive);
+      });
     });
   }, []);
 
   return mainChannelId ? (
-    <MainChannelTopLoader mainChannelId={mainChannelId} />
+    channel ? (
+      <LiveControlPanel channelHavingClosestLive={channel} />
+    ) : (
+      <MainChannelTopLoader mainChannelId={mainChannelId} />
+    )
   ) : (
     <ChannelRegistrationLoader />
   );
