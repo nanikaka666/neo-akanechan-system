fe75ff4044aac97b51bbd2a779e2359da9533143
nanikaka <nanikaka666@gmail.com>
2025-11-17 12:27:17 +0900

    enable to confirm scroll at the end of comment viewer

--- src/renderer/mainWindow/components/liveControl/CommentViewer.tsx
+++ src/renderer/mainWindow/components/liveControl/CommentViewer.tsx
@@ -1,9 +1,13 @@
-import { useEffect, useState } from "react";
-import { Virtuoso } from "react-virtuoso";
+import { useEffect, useMemo, useState } from "react";
+import { ListRange, Virtuoso } from "react-virtuoso";
 import { ChatItemText } from "youtube-livechat-emitter/dist/src/types/liveChat";
 
 export function CommentViewer() {
   const [textChats, setTextChats] = useState<ChatItemText[]>([]);
+  const [range, setRange] = useState<ListRange>({ startIndex: 0, endIndex: 0 });
+  const atTheEnd = useMemo(() => {
+    return textChats.length === 0 || textChats.length - 1 === range.endIndex;
+  }, [textChats, range]);
 
   useEffect(() => {
     window.ipcApi.registerTextChatsListener((e, newTextChats) => {
@@ -14,9 +18,14 @@ export function CommentViewer() {
 
   return (
     <div>
+      <div style={{ position: "absolute", top: 0, right: 0 }}>
+        {range.startIndex} - {range.endIndex} / {textChats.length}
+        At the End: {atTheEnd ? "YES" : "NO"}
+      </div>
       <Virtuoso
         data={textChats}
         style={{ height: `calc(100vh)` }}
+        rangeChanged={setRange}
         itemContent={(index, textChat) => {
           return (
             <div>
