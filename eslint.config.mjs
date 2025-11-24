86215d63ba680219276003fe52c1b5090d7901c1
nanikaka <nanikaka666@gmail.com>
2025-11-24 16:26:20 +0900

    turn on eslint-plugin-react-hooks

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -7,6 +7,7 @@ import { fileURLToPath } from "node:url";
 import js from "@eslint/js";
 import { FlatCompat } from "@eslint/eslintrc";
 import reactPlugin from "eslint-plugin-react";
+import reactHooksPlugin from "eslint-plugin-react-hooks";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -39,6 +40,7 @@ export default defineConfig([
       ),
       reactPlugin.configs.flat.recommended,
       reactPlugin.configs.flat["jsx-runtime"], // if React +17, need it
+      reactHooksPlugin.configs.flat.recommended,
     ],
 
     settings: {
