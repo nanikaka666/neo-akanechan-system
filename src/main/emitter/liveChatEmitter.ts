718cbf24b5b14401e962ad26e6af1dc36fd9764b
nanikaka <nanikaka666@gmail.com>
2026-01-08 03:32:25 +0900

    improve liveChatEmitter

--- src/main/emitter/liveChatEmitter.ts
+++ src/main/emitter/liveChatEmitter.ts
@@ -4,10 +4,51 @@ import { ActiveLiveChatId, LiveChatId } from "../youtubeApi/model";
 import { V3DataLiveChatMessageServiceClient } from "../grpc/generated/proto/stream_list_grpc_pb";
 import { credentials, Metadata } from "@grpc/grpc-js/build/src";
 import {
+  LiveChatMessage,
   LiveChatMessageListRequest,
   LiveChatMessageListResponse,
+  LiveChatMessageSnippet,
 } from "../grpc/generated/proto/stream_list_pb";
 import { getAccessToken } from "../auth/google";
+
+type ItemType =
+  | "invalid"
+  | "textMessageEvent"
+  | "tombstone"
+  | "fanFundingEvent"
+  | "chatEndedEvent"
+  | "sponsorOnlyModeStartedEvent"
+  | "sponsorOnlyModeEndedEvent"
+  | "newSponsorEvent"
+  | "memberMilestoneChatEvent"
+  | "membershipGiftingEvent"
+  | "giftMembershipReceivedEvent"
+  | "messageDeletedEvent"
+  | "messageRetractedEvent"
+  | "userBannedEvent"
+  | "superChatEvent"
+  | "superStickerEvent"
+  | "pollEvent";
+
+const itemTypeMap: Record<ItemType, number> = {
+  invalid: 0,
+  textMessageEvent: 1,
+  tombstone: 2,
+  fanFundingEvent: 3,
+  chatEndedEvent: 4,
+  sponsorOnlyModeStartedEvent: 5,
+  sponsorOnlyModeEndedEvent: 6,
+  newSponsorEvent: 7,
+  memberMilestoneChatEvent: 17,
+  membershipGiftingEvent: 18,
+  giftMembershipReceivedEvent: 19,
+  messageDeletedEvent: 8,
+  messageRetractedEvent: 9,
+  userBannedEvent: 10,
+  superChatEvent: 15,
+  superStickerEvent: 16,
+  pollEvent: 20,
+};
 
 export class LiveChatEmitter extends (EventEmitter as new () => TypedEmitter<LiveChatEvent>) {
   readonly #liveChatId: LiveChatId | ActiveLiveChatId;
@@ -39,56 +80,126 @@ export class LiveChatEmitter extends (EventEmitter as new () => TypedEmitter<Liv
     return [request, metadata] as const;
   }
 
+  #checkItemType(item: LiveChatMessage, expected: number) {
+    if (item.getSnippet()?.getType() !== expected) {
+      throw new Error(
+        `LiveChat Handler is wrong. given: ${item.getSnippet()?.getType()}, Expected: ${expected}`,
+      );
+    }
+  }
+
+  #handleTextMessageEvent(item: LiveChatMessage) {
+    this.#checkItemType(item, itemTypeMap.textMessageEvent);
+
+    let data = { id: item.getId() };
+
+    const snippet = item.getSnippet();
+
+    if (snippet) {
+      data = {
+        ...data,
+        ...{
+          authorChannelId: snippet.getAuthorChannelId(),
+          publishedAt: snippet.getPublishedAt() ? new Date(snippet.getPublishedAt()!) : undefined,
+          displayMessage: snippet.getDisplayMessage(),
+          messageText: snippet.getTextMessageDetails()?.getMessageText(),
+        },
+      };
+    }
+    const authorDetails = item.getAuthorDetails();
+    if (authorDetails) {
+      data = {
+        ...data,
+        ...{
+          channelId: authorDetails.getChannelId(),
+          channelUrl: authorDetails.getChannelUrl(),
+          displayName: authorDetails.getDisplayName(),
+          profileImageUrl: authorDetails.getProfileImageUrl(),
+          isVerified: authorDetails.getIsVerified(),
+          isChatOwner: authorDetails.getIsChatOwner(),
+          isChatSponsor: authorDetails.getIsChatSponsor(),
+          isChatModerator: authorDetails.getIsChatModerator(),
+        },
+      };
+    }
+    console.log(data);
+  }
+  #handleNewMembershipsEvent(item: LiveChatMessage) {
+    this.#checkItemType(item, itemTypeMap.newSponsorEvent);
+  }
+  #handleMessageDeletedEvent(item: LiveChatMessage) {
+    this.#checkItemType(item, itemTypeMap.messageDeletedEvent);
+  }
+  #handleUserBannedEvent(item: LiveChatMessage) {
+    this.#checkItemType(item, itemTypeMap.userBannedEvent);
+  }
+  #handleSuperChatEvent(item: LiveChatMessage) {
+    this.#checkItemType(item, itemTypeMap.superChatEvent);
+  }
+  #handleSuperStickerEvent(item: LiveChatMessage) {
+    this.#checkItemType(item, itemTypeMap.superStickerEvent);
+  }
+  #handleMemberMilestoneChatEvent(item: LiveChatMessage) {
+    this.#checkItemType(item, itemTypeMap.memberMilestoneChatEvent);
+  }
+  #handleMembershipGiftEvent(item: LiveChatMessage) {
+    this.#checkItemType(item, itemTypeMap.membershipGiftingEvent);
+  }
+  #handleMembershipGiftReceivedEvent(item: LiveChatMessage) {
+    this.#checkItemType(item, itemTypeMap.giftMembershipReceivedEvent);
+  }
+
   async #execute() {
     let num = 0;
-    const typeMap: Record<string, string> = {
-      0: "Invalid",
-      1: "TEXT_MESSAGE_EVENT: 1;",
-      2: "TOMBSTONE: 2;",
-      3: "FAN_FUNDING_EVENT: 3;",
-      4: "CHAT_ENDED_EVENT: 4;",
-      5: "SPONSOR_ONLY_MODE_STARTED_EVENT: 5;",
-      6: "SPONSOR_ONLY_MODE_ENDED_EVENT: 6;",
-      7: "NEW_SPONSOR_EVENT: 7;",
-      17: "MEMBER_MILESTONE_CHAT_EVENT: 17;",
-      18: "MEMBERSHIP_GIFTING_EVENT: 18;",
-      19: "GIFT_MEMBERSHIP_RECEIVED_EVENT: 19;",
-      8: "MESSAGE_DELETED_EVENT: 8;",
-      9: "MESSAGE_RETRACTED_EVENT: 9;",
-      10: "USER_BANNED_EVENT: 10;",
-      15: "SUPER_CHAT_EVENT: 15;",
-      16: "SUPER_STICKER_EVENT: 16;",
-      20: "POLL_EVENT: 20;",
-    };
     while (this.#isActivated) {
       console.log(`Call Num: (${++num})`);
       const [request, metadata] = await this.#buildParams();
       await this.#client.streamList(request, metadata).forEach((data) => {
         const res = data as LiveChatMessageListResponse;
 
-        // export interface TypeMap {
-        //   INVALID_TYPE: 0;
-        //   TEXT_MESSAGE_EVENT: 1;
-        //   TOMBSTONE: 2;
-        //   FAN_FUNDING_EVENT: 3;
-        //   CHAT_ENDED_EVENT: 4;
-        //   SPONSOR_ONLY_MODE_STARTED_EVENT: 5;
-        //   SPONSOR_ONLY_MODE_ENDED_EVENT: 6;
-        //   NEW_SPONSOR_EVENT: 7;
-        //   MEMBER_MILESTONE_CHAT_EVENT: 17;
-        //   MEMBERSHIP_GIFTING_EVENT: 18;
-        //   GIFT_MEMBERSHIP_RECEIVED_EVENT: 19;
-        //   MESSAGE_DELETED_EVENT: 8;
-        //   MESSAGE_RETRACTED_EVENT: 9;
-        //   USER_BANNED_EVENT: 10;
-        //   SUPER_CHAT_EVENT: 15;
-        //   SUPER_STICKER_EVENT: 16;
-        //   POLL_EVENT: 20;
-        // }
+        console.log(
+          `Total: ${res.getPageInfo()?.getTotalResults()}, Per page: ${res.getPageInfo()?.getResultsPerPage()}`,
+        );
+
         res.getItemsList().forEach((item) => {
-          console.log(
-            `[${typeMap[item.getSnippet()?.getType() + ""]}] (${item.getAuthorDetails()?.getDisplayName()}) ${item.getSnippet()?.getDisplayMessage()}`,
-          );
+          const itemType = item.getSnippet()?.getType();
+          if (itemType === itemTypeMap.invalid) {
+            // do nothing.
+          } else if (itemType === itemTypeMap.textMessageEvent) {
+            this.#handleTextMessageEvent(item);
+          } else if (itemType === itemTypeMap.tombstone) {
+            // do nothing.
+          } else if (itemType === itemTypeMap.fanFundingEvent) {
+            // do nothing.
+          } else if (itemType === itemTypeMap.chatEndedEvent) {
+            // do nothing.
+          } else if (itemType === itemTypeMap.sponsorOnlyModeStartedEvent) {
+            // do nothing.
+          } else if (itemType === itemTypeMap.sponsorOnlyModeEndedEvent) {
+            // do nothing.
+          } else if (itemType === itemTypeMap.newSponsorEvent) {
+            this.#handleNewMembershipsEvent(item);
+          } else if (itemType === itemTypeMap.messageDeletedEvent) {
+            this.#handleMessageDeletedEvent(item);
+          } else if (itemType === itemTypeMap.messageRetractedEvent) {
+            // do nothing.
+          } else if (itemType === itemTypeMap.userBannedEvent) {
+            this.#handleUserBannedEvent(item);
+          } else if (itemType === itemTypeMap.superChatEvent) {
+            this.#handleSuperChatEvent(item);
+          } else if (itemType === itemTypeMap.superStickerEvent) {
+            this.#handleSuperStickerEvent(item);
+          } else if (itemType === itemTypeMap.memberMilestoneChatEvent) {
+            this.#handleMemberMilestoneChatEvent(item);
+          } else if (itemType === itemTypeMap.membershipGiftingEvent) {
+            this.#handleMembershipGiftEvent(item);
+          } else if (itemType === itemTypeMap.giftMembershipReceivedEvent) {
+            this.#handleMembershipGiftReceivedEvent(item);
+          } else if (itemType === itemTypeMap.pollEvent) {
+            // do nothing.
+          } else {
+            this.emit("error", new Error(`Unknown Item Type Detected. ${itemType}`));
+          }
         });
 
         const nextPageToken = res.getNextPageToken();
