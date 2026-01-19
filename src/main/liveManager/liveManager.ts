75ecbdcdedff267f80007f129cf69e36330201cb
nanikaka <nanikaka666@gmail.com>
2026-01-19 21:11:05 +0900

    implements basically LiveManager

---
+++ src/main/liveManager/liveManager.ts
@@ -0,0 +1,45 @@
+import { LiveLaunchProperties } from "../../types/liveLaunchProperties";
+import { ChannelStatisticsEmitter } from "../emitter/channelStatisticsEmitter";
+import { DataSource } from "./dataSource";
+import { Processor } from "./processor";
+
+export class LiveManager {
+  readonly #liveLaunchProperties: LiveLaunchProperties;
+  readonly #dataSource: DataSource;
+  readonly #processor: Processor;
+  readonly #channelDataFetcher: ChannelStatisticsEmitter;
+  constructor(
+    liveLaunchProperties: LiveLaunchProperties,
+    dataSource: DataSource,
+    processor: Processor,
+    channelDataFetcher: ChannelStatisticsEmitter,
+  ) {
+    this.#liveLaunchProperties = liveLaunchProperties;
+    this.#dataSource = dataSource;
+    this.#processor = processor;
+    this.#channelDataFetcher = channelDataFetcher;
+  }
+  async setup() {
+    await this.#setupChannelDataFetcher();
+  }
+
+  async #setupChannelDataFetcher() {
+    this.#channelDataFetcher.removeAllListeners();
+    this.#channelDataFetcher.once("start", (initialValue) => {
+      console.log("ChannelDataFetcher started.");
+      this.#processor.subscriberCount(initialValue);
+    });
+    this.#channelDataFetcher.once("end", () => {
+      console.log("ChannelDataFetcher finished.");
+    });
+    this.#channelDataFetcher.on("error", console.log);
+    this.#channelDataFetcher.on("nextSubscriberCount", (nextSubscriberCount) =>
+      this.#processor.subscriberCount(nextSubscriberCount),
+    );
+    await this.#channelDataFetcher.start();
+  }
+
+  close() {
+    this.#channelDataFetcher.close();
+  }
+}
