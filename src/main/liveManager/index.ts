acb3352a6e10416f95a85797c4542b7a37f6dde7
nanikaka <nanikaka666@gmail.com>
2026-01-19 21:11:18 +0900

    put setupLiveManager

---
+++ src/main/liveManager/index.ts
@@ -0,0 +1,32 @@
+import { WebContents } from "electron";
+import { DataSource } from "./dataSource";
+import { LiveManager } from "./liveManager";
+import { LcpDataTransfer } from "./transfer/lcpDataTransfer";
+import { Processor } from "./processor";
+import { LiveLaunchProperties } from "../../types/liveLaunchProperties";
+import { ChannelStatisticsEmitter } from "../emitter/channelStatisticsEmitter";
+
+let liveManager: LiveManager | undefined;
+
+export async function setupLiveManager(
+  webContents: WebContents,
+  liveLaunchProperties: LiveLaunchProperties,
+) {
+  if (liveManager !== undefined) {
+    liveManager.close();
+    liveManager = undefined;
+  }
+  const dataSource = new DataSource();
+  const lcpDataTransfer = new LcpDataTransfer(webContents, dataSource);
+  const processor = new Processor(liveLaunchProperties, dataSource, lcpDataTransfer);
+  const channelDataFetcher = new ChannelStatisticsEmitter(liveLaunchProperties.channel.id);
+  liveManager = new LiveManager(liveLaunchProperties, dataSource, processor, channelDataFetcher);
+  await liveManager.setup();
+}
+
+export function getLiveManager() {
+  if (liveManager === undefined) {
+    throw new Error("LiveManager has not been setup.");
+  }
+  return liveManager;
+}
