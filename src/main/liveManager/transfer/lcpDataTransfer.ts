9d21efbcbfe61293328b4b86e56025f3204e810a
nanikaka <nanikaka666@gmail.com>
2026-01-19 21:10:06 +0900

    implements basically LcpDataTransfer

---
+++ src/main/liveManager/transfer/lcpDataTransfer.ts
@@ -0,0 +1,19 @@
+import { WebContents } from "electron";
+import { DataSource } from "../dataSource";
+import { WebContentsWrapper } from "../../../main/webContentsWrapper";
+
+export class LcpDataTransfer {
+  readonly #webContents: WebContents;
+  readonly #dataSource: DataSource;
+  constructor(webContents: WebContents, dataSource: DataSource) {
+    this.#webContents = webContents;
+    this.#dataSource = dataSource;
+  }
+  syncLiveStatistics() {
+    WebContentsWrapper.send(
+      this.#webContents,
+      "tellLiveStatistics",
+      this.#dataSource.getLiveStatistics(),
+    );
+  }
+}
