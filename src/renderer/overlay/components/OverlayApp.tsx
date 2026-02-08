f479f9ceff2f0fe7c1eca12bb14b0dd4d93573e7
nanikaka <nanikaka666@gmail.com>
2026-02-09 01:20:07 +0900

    use CarouselManager

--- src/renderer/overlay/components/OverlayApp.tsx
+++ src/renderer/overlay/components/OverlayApp.tsx
@@ -1,9 +1,22 @@
+import { CarouselManager } from "./CarouselManager";
+import { Clock } from "./Clock";
 import { PoppingManager } from "./PoppingManager";
+import { LikeCountIndicator } from "./LikeCountIndicator";
+import { ViewerCountIndicator } from "./ViewerCountIndicator";
+import { SubscriberCountIndicator } from "./SubscriberCountIndicator";
 
 export function OverlayApp() {
   return (
     <div>
       <PoppingManager />
+      <CarouselManager
+        items={[
+          <LikeCountIndicator key={1} gaugeLevel={1} />,
+          <ViewerCountIndicator key={2} gaugeLevel={3} />,
+          <SubscriberCountIndicator key={3} gaugeLevel={5} />,
+          <Clock key={4} />,
+        ]}
+      />
     </div>
   );
 }
