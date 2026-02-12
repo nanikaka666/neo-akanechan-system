4fa1704e146b4ed727b72571f1a5bd9adc1b41e9
nanikaka <nanikaka666@gmail.com>
2026-02-12 15:49:02 +0900

    Overlay receives LiveSettings

--- src/renderer/overlay/components/OverlayApp.tsx
+++ src/renderer/overlay/components/OverlayApp.tsx
@@ -4,19 +4,30 @@ import { PoppingManager } from "./PoppingManager";
 import { LikeCountIndicator } from "./LikeCountIndicator";
 import { ViewerCountIndicator } from "./ViewerCountIndicator";
 import { SubscriberCountIndicator } from "./SubscriberCountIndicator";
+import { useEffect, useState } from "react";
+import { LiveSettings } from "../../../types/liveSettings";
 
 export function OverlayApp() {
+  const [liveSettings, setLiveSettings] = useState<LiveSettings>();
+  useEffect(() => {
+    window.ipcApi.registerLiveSettingsListener((e, liveSettings) =>
+      setLiveSettings((_) => liveSettings),
+    );
+  }, []);
+
   return (
     <div>
       <PoppingManager />
-      <CarouselManager
-        items={[
-          <LikeCountIndicator key={1} gaugeLevel={1} />,
-          <ViewerCountIndicator key={2} gaugeLevel={3} />,
-          <SubscriberCountIndicator key={3} gaugeLevel={5} />,
-          <Clock key={4} />,
-        ]}
-      />
+      {liveSettings && (
+        <CarouselManager
+          items={[
+            <LikeCountIndicator key={1} gaugeLevel={1} />,
+            <ViewerCountIndicator key={2} gaugeLevel={3} />,
+            <SubscriberCountIndicator key={3} gaugeLevel={5} />,
+            <Clock key={4} />,
+          ]}
+        />
+      )}
     </div>
   );
 }
