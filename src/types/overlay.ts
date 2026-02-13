61bc233e80f2f281ee55dbb318cb2620b0320208
nanikaka <nanikaka666@gmail.com>
2026-02-14 08:48:43 +0900

    define OverlayEvent base

--- src/types/overlay.ts
+++ src/types/overlay.ts
@@ -2,3 +2,28 @@ export interface PointInfoFromMainProcess {
   img: string;
   point: number;
 }
+
+export type OverlayEvent =
+  | NoEvent
+  | LikeCountLevelPromotionEvent
+  | ViewerCountLevelPromotionEvent
+  | SubscriberCountGoalAchivementEvent;
+
+export interface NoEvent {
+  type: "noEvent";
+}
+
+export interface LikeCountLevelPromotionEvent {
+  type: "likeCountLevelPromotion";
+  points: PointInfoFromMainProcess[];
+}
+
+export interface ViewerCountLevelPromotionEvent {
+  type: "viewerCountLevelPromotion";
+  points: PointInfoFromMainProcess[];
+}
+
+export interface SubscriberCountGoalAchivementEvent {
+  type: "subscriberCountGoalAchivement";
+  points: PointInfoFromMainProcess[];
+}
