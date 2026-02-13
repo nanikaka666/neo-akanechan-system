f2824bba570cc2a9e03ccd76d8bfc131219b1b73
nanikaka <nanikaka666@gmail.com>
2026-02-13 18:55:14 +0900

    refactor: extract as hooks

--- src/renderer/overlay/components/OverlayApp.tsx
+++ src/renderer/overlay/components/OverlayApp.tsx
@@ -4,77 +4,46 @@ import { PoppingManager } from "./PoppingManager";
 import { LikeCountIndicator } from "./LikeCountIndicator";
 import { ViewerCountIndicator } from "./ViewerCountIndicator";
 import { SubscriberCountIndicator } from "./SubscriberCountIndicator";
-import { useEffect, useState } from "react";
-import { LiveSettings } from "../../../types/liveSettings";
-import { LiveStatistics } from "../../../types/liveStatistics";
+import { useEffect } from "react";
+import { useLiveSettings } from "./hooks/useLiveSettings";
+import { useLiveStatistics } from "./hooks/useLiveStatistics";
 
 export function OverlayApp() {
-  const [liveSettings, setLiveSettings] = useState<LiveSettings>();
-  const [liveStatistics, setLiveStatistics] = useState<LiveStatistics>({
-    currentLikeCount: 0,
-    maxLikeCount: 0,
-    currentLiveViewCount: 0,
-    maxLiveViewCount: 0,
-    textChatCount: 0,
-    superChatCount: 0,
-    superStickerCount: 0,
-    chatUUCount: 0,
-    currentSubscriberCount: 0,
-    maxSubscriberCount: 0,
-    newMembershipsCount: 0,
-    membershipMilestoneCount: 0,
-    giftCount: 0,
-    redemptionGiftCount: 0,
-    stocksCount: 0,
-  });
+  const liveSettings = useLiveSettings();
+  const liveStatistics = useLiveStatistics();
   useEffect(() => {
-    const liveSettingsListenerRemover = window.ipcApi.registerLiveSettingsListener(
-      (e, liveSettings) => {
-        setLiveSettings((_) => liveSettings);
-      },
-    );
+    // to rewrite default settings by latest LiveSettings
     window.ipcApi.requestSyncLiveSettings();
-    const liveStatisticsListenerRemover = window.ipcApi.registerLiveStatisticsListener(
-      (e, liveStatistics) => {
-        setLiveStatistics((_) => liveStatistics);
-      },
-    );
-    return () => {
-      liveSettingsListenerRemover();
-      liveStatisticsListenerRemover();
-    };
   }, []);
 
   return (
     <div>
       <PoppingManager />
-      {liveSettings && (
-        <CarouselManager
-          items={[
-            <LikeCountIndicator
-              key={1}
-              gaugeLevel={1}
-              goal={liveSettings.likeCountGoal}
-              currentValue={liveStatistics.currentLikeCount}
-              maxValueSoFar={liveStatistics.maxLikeCount}
-            />,
-            <ViewerCountIndicator
-              key={2}
-              gaugeLevel={1}
-              goal={liveSettings.viewerCountGoal}
-              currentValue={liveStatistics.currentLiveViewCount}
-              maxValueSoFar={liveStatistics.maxLiveViewCount}
-            />,
-            <SubscriberCountIndicator
-              key={3}
-              goalValue={liveSettings.subscriberCountGoal}
-              currentValue={liveStatistics.currentSubscriberCount}
-              maxValueSoFar={liveStatistics.maxSubscriberCount}
-            />,
-            <Clock key={4} />,
-          ]}
-        />
-      )}
+      <CarouselManager
+        items={[
+          <LikeCountIndicator
+            key={1}
+            gaugeLevel={1}
+            goal={liveSettings.likeCountGoal}
+            currentValue={liveStatistics.currentLikeCount}
+            maxValueSoFar={liveStatistics.maxLikeCount}
+          />,
+          <ViewerCountIndicator
+            key={2}
+            gaugeLevel={1}
+            goal={liveSettings.viewerCountGoal}
+            currentValue={liveStatistics.currentLiveViewCount}
+            maxValueSoFar={liveStatistics.maxLiveViewCount}
+          />,
+          <SubscriberCountIndicator
+            key={3}
+            goalValue={liveSettings.subscriberCountGoal}
+            currentValue={liveStatistics.currentSubscriberCount}
+            maxValueSoFar={liveStatistics.maxSubscriberCount}
+          />,
+          <Clock key={4} />,
+        ]}
+      />
     </div>
   );
 }
