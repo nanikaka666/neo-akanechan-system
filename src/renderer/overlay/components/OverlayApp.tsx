d99f03e8f4a7812432f2e33699094a2c754ae170
nanikaka <nanikaka666@gmail.com>
2026-02-15 16:27:45 +0900

    use OnDemandPoppingManager when like count goal promotion

--- src/renderer/overlay/components/OverlayApp.tsx
+++ src/renderer/overlay/components/OverlayApp.tsx
@@ -4,37 +4,65 @@ import { PoppingManager } from "./PoppingManager";
 import { LikeCountIndicator } from "./LikeCountIndicator";
 import { ViewerCountIndicator } from "./ViewerCountIndicator";
 import { SubscriberCountIndicator } from "./SubscriberCountIndicator";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useLiveSettings } from "./hooks/useLiveSettings";
 import { useLiveStatistics } from "./hooks/useLiveStatistics";
 import { useOverlayEvent } from "./hooks/useOverlayEvent";
+import { GoalsLevel, GoalsStatus } from "../../../types/goals";
+import { OnDemandPoppingManager } from "./OnDemandPoppingManager";
 
 export function OverlayApp() {
   const liveSettings = useLiveSettings();
   const liveStatistics = useLiveStatistics();
   const [overlayEvent, overlayEventResetFunc] = useOverlayEvent();
+  const [likeCountLevel, setLikeCountLevel] = useState<GoalsStatus>({
+    type: "inProgress",
+    currentLevel: 1,
+  });
 
   useEffect(() => {
     // to rewrite default settings by latest LiveSettings
     window.ipcApi.requestSyncLiveSettings();
   }, []);
 
-  useEffect(() => {
-    console.log("useEffect with ", overlayEvent.type);
-    if (overlayEvent.type === "noEvent") {
-      return;
-    }
-    // todo: process each events
-  }, [overlayEvent, overlayEventResetFunc]);
+  const poppingOnDemand =
+    overlayEvent.type === "likeCountLevelPromotion"
+      ? {
+          buffer: overlayEvent.points,
+          funcOnLastAnimationEnded: () => {
+            console.log("Do funcOnLastAnimationEnded");
+            overlayEventResetFunc();
+            if (likeCountLevel.type === "inProgress") {
+              if (likeCountLevel.currentLevel === liveSettings.likeCountGoal.maxLevel) {
+                setLikeCountLevel(() => {
+                  return { type: "accomplished" };
+                });
+              } else {
+                setLikeCountLevel(() => {
+                  return {
+                    type: "inProgress",
+                    currentLevel: (likeCountLevel.currentLevel + 1) as GoalsLevel,
+                  };
+                });
+              }
+            }
+          },
+        }
+      : undefined;
 
   return (
     <div>
       <PoppingManager />
+      {poppingOnDemand && <OnDemandPoppingManager onDemand={poppingOnDemand} />}
       <CarouselManager
         items={[
           <LikeCountIndicator
             key={1}
-            gaugeLevel={1}
+            gaugeLevel={
+              likeCountLevel.type === "accomplished"
+                ? liveSettings.likeCountGoal.maxLevel
+                : likeCountLevel.currentLevel
+            }
             goal={liveSettings.likeCountGoal}
             currentValue={liveStatistics.currentLikeCount}
             maxValueSoFar={liveStatistics.maxLikeCount}
