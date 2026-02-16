1709bee4cfe65d1190c3dcc5d56747829ae7f82a
nanikaka <nanikaka666@gmail.com>
2026-02-16 20:05:44 +0900

    implements useAllGoalsStatus hook

---
+++ src/renderer/mainWindow/components/hooks/useAllGoalsStatus.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { AllGoalsStatus } from "../../../../types/goals";
+
+export function useAllGoalsStatus() {
+  const [allGoalStatus, setAllGoalStatus] = useState<AllGoalsStatus>({
+    likeCountStatus: { type: "inProgress", currentLevel: 1 },
+    viewerCountStatus: { type: "inProgress", currentLevel: 1 },
+    isSubscriberCountGoalAccomplished: false,
+  });
+
+  useEffect(() => {
+    const remover = window.ipcApi.registerAllGoalStatus((_, status) => {
+      setAllGoalStatus((_) => status);
+    });
+    return () => remover();
+  }, []);
+
+  return allGoalStatus;
+}
