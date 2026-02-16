20cde2a79d06518ddc188d58e21bfbdc5a39b89d
nanikaka <nanikaka666@gmail.com>
2026-02-16 19:44:06 +0900

    refactor: extract as useLiveStatistics hook

--- src/renderer/mainWindow/components/liveControlPanel/SideBarInfoArea.tsx
+++ src/renderer/mainWindow/components/liveControlPanel/SideBarInfoArea.tsx
@@ -1,31 +1,7 @@
-import { useEffect, useState } from "react";
-import { LiveStatistics } from "../../../../types/liveStatistics";
+import { useLiveStatistics } from "../hooks/useLiveStatistics";
 
 export function SideBarInfoArea() {
-  const [liveStatistics, setLiveStatistics] = useState<LiveStatistics>({
-    currentLikeCount: 0,
-    maxLikeCount: 0,
-    currentLiveViewCount: 0,
-    maxLiveViewCount: 0,
-    textChatCount: 0,
-    superChatCount: 0,
-    superStickerCount: 0,
-    chatUUCount: 0,
-    currentSubscriberCount: 0,
-    maxSubscriberCount: 0,
-    newMembershipsCount: 0,
-    membershipMilestoneCount: 0,
-    giftCount: 0,
-    redemptionGiftCount: 0,
-    stocksCount: 0,
-  });
-
-  useEffect(() => {
-    const remover = window.ipcApi.registerLiveStatisticsListener((e, newLiveStatistics) => {
-      setLiveStatistics((_) => newLiveStatistics);
-    });
-    return () => remover();
-  }, []);
+  const liveStatistics = useLiveStatistics();
 
   return (
     <div>
