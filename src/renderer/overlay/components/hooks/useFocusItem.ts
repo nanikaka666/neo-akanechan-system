3571c3782b740aec5355380c8d2ec38e25dd1c43
nanikaka <nanikaka666@gmail.com>
2026-02-21 21:34:58 +0900

    listen latest FocusViewItem in custom hook

--- src/renderer/overlay/components/hooks/useFocusItem.ts
+++ src/renderer/overlay/components/hooks/useFocusItem.ts
@@ -1,8 +1,15 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { FocusViewItem } from "../../../../types/focusView";
 
 export function useFocusItem() {
   const [focusItem, setFocusItem] = useState<FocusViewItem | undefined>();
+
+  useEffect(() => {
+    const remover = window.ipcApi.registerFocusItemListener((_, item) => {
+      setFocusItem((_) => item);
+    });
+    return () => remover();
+  }, []);
 
   return focusItem;
 }
