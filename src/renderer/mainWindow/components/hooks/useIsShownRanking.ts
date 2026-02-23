99dfbbf8ca91f8e4bf2a3e9107317451d6a44131
nanikaka <nanikaka666@gmail.com>
2026-02-24 03:29:22 +0900

    implements useIsShownRanking custom hook

---
+++ src/renderer/mainWindow/components/hooks/useIsShownRanking.ts
@@ -0,0 +1,13 @@
+import { useEffect, useState } from "react";
+
+export function useIsShownRanking() {
+  const [isShown, setIsShown] = useState(false);
+  useEffect(() => {
+    const remover = window.ipcApi.registerIsShownRankingListener((_, nextIsShown) => {
+      setIsShown((_) => nextIsShown);
+    });
+    return () => remover();
+  }, []);
+
+  return isShown;
+}
