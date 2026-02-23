7026eb143bae0b5f9aef0e7a06eb02a842cdf370
nanikaka <nanikaka666@gmail.com>
2026-02-24 02:18:32 +0900

    listen ranking data at RankingView component

--- src/renderer/overlay/components/RankingView.tsx
+++ src/renderer/overlay/components/RankingView.tsx
@@ -1,10 +1,18 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Ranking } from "./Ranking";
 import { RankingOthers } from "./RankingOthers";
 import { PariticipantPointRankings } from "../../../types/participantPoint";
 
 export function RankingView() {
   const [ranking, setRanking] = useState<PariticipantPointRankings | undefined>();
+
+  useEffect(() => {
+    const remover = window.ipcApi.registerRankingViewListener((_, maybeRanking) => {
+      setRanking((_) => maybeRanking);
+    });
+    return () => remover();
+  }, []);
+
   return (
     ranking && (
       <>
