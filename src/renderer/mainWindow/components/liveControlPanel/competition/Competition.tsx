647943eb4c0831e79e15933544507be4dd6956ff
nanikaka <nanikaka666@gmail.com>
2026-03-01 16:17:04 +0900

    Competition component listens latest CompetitionStatus

--- src/renderer/mainWindow/components/liveControlPanel/competition/Competition.tsx
+++ src/renderer/mainWindow/components/liveControlPanel/competition/Competition.tsx
@@ -1,5 +1,21 @@
+import { useEffect, useState } from "react";
 import { NotHeld } from "./NotHeld";
+import { CompetitionStatus } from "../../../../../types/competition";
 
 export function Competition() {
-  return <NotHeld />;
+  const [status, setStatus] = useState<CompetitionStatus>({ type: "notHeld" });
+
+  useEffect(() => {
+    const remover = window.ipcApi.registerCompetitionStatusListener((_, newStatus) => {
+      setStatus((_) => newStatus);
+    });
+    return () => remover();
+  }, []);
+  return status.type === "notHeld" ? (
+    <NotHeld />
+  ) : status.type === "held" ? (
+    <div>Held</div>
+  ) : (
+    <div>Entry Closed</div>
+  );
 }
