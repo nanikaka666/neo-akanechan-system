c1cc1dd4550af442912f0b61985ef7b4efaa626d
nanikaka <nanikaka666@gmail.com>
2026-03-02 12:40:27 +0900

    implements AnswerDecisionForm component

---
+++ src/renderer/mainWindow/components/liveControlPanel/competition/AnswerDecisionForm.tsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+import { OptionLabel } from "../../../../../types/competition";
+
+export interface AnswerDecisionFormProps {
+  options: Map<OptionLabel, string>;
+}
+
+export function AnswerDecisionForm({ options }: AnswerDecisionFormProps) {
+  const [answer, setAnswer] = useState<OptionLabel>("a");
+  const [disabled, setDisabled] = useState(false);
+
+  return (
+    <div>
+      <select
+        onChange={(e) => {
+          e.preventDefault();
+          setAnswer((_) => e.target.value as OptionLabel);
+        }}
+      >
+        {Array.from(options.keys()).map((label) => {
+          return (
+            <option key={label} value={label}>
+              {label}: {options.get(label)}
+            </option>
+          );
+        })}
+      </select>
+      <button
+        disabled={disabled}
+        onClick={(e) => {
+          e.preventDefault();
+          setDisabled((_) => true);
+          window.ipcApi.requestAnswerDecision(answer).then(() => {
+            setDisabled((_) => false);
+          });
+        }}
+      >
+        正解を確定する
+      </button>
+    </div>
+  );
+}
