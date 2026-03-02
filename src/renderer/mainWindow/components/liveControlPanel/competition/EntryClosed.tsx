ec5bc23888bd748f14610736b451c1106c85ebcb
nanikaka <nanikaka666@gmail.com>
2026-03-02 12:41:00 +0900

    implements EntryClosed component

---
+++ src/renderer/mainWindow/components/liveControlPanel/competition/EntryClosed.tsx
@@ -0,0 +1,20 @@
+import { EntryClosedCompetition } from "../../../../../types/competition";
+import { AbortButton } from "./AbortButton";
+import { AnswerDecisionForm } from "./AnswerDecisionForm";
+import { BetStatusView } from "./BetStatusView";
+
+export interface EntryClosedProps {
+  status: EntryClosedCompetition;
+}
+
+export function EntryClosed({ status }: EntryClosedProps) {
+  return (
+    <div key={status.settings.competitionId}>
+      <div>問題: {status.settings.question}</div>
+      <div>受付締切済み</div>
+      <BetStatusView status={status} />
+      <AnswerDecisionForm options={status.settings.options} />
+      <AbortButton />
+    </div>
+  );
+}
