06c503e199f714e19184e93be8f256a7c9467fee
nanikaka <nanikaka666@gmail.com>
2026-03-04 09:25:21 +0900

    implements HeldView component

---
+++ src/renderer/overlay/components/HeldView.tsx
@@ -0,0 +1,22 @@
+import { HeldCompetition } from "../../../types/competition";
+
+import { CompetitionStatusView } from "./CompetitionStatusView";
+
+interface HeldViewProps {
+  status: HeldCompetition;
+}
+
+export function HeldView({ status }: HeldViewProps) {
+  return (
+    <div>
+      <CompetitionStatusView status={status} />
+
+      <div>
+        正解だと思う選択肢を選び、チャット欄にスラッシュ(/)をつけて書き込んでください。例:
+        選択肢AにBetするなら「/A」or「/a」です。
+      </div>
+
+      <div>参加締切: {status.settings.scheduledClosedAt.toLocaleString().split(" ")[1]}</div>
+    </div>
+  );
+}
