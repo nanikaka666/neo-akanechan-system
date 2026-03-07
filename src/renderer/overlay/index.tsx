84de7aade5cff2963b9eefd45df0a440e592134f
nanikaka <nanikaka666@gmail.com>
2026-03-07 13:43:31 +0900

    refactor: put back to index.css

--- src/renderer/overlay/index.tsx
+++ src/renderer/overlay/index.tsx
@@ -1,5 +1,5 @@
 import { createRoot } from "react-dom/client";
-import "./css/index.css";
+import "./index.css";
 import { OverlayApp } from "./components/OverlayApp";
 
 createRoot(document.getElementById("root")!).render(<OverlayApp></OverlayApp>);
