ac55b389d88c182087cae329f3a879244d70d0f8
nanikaka <nanikaka666@gmail.com>
2026-03-07 17:47:01 +0900

    adjust Clock component

--- src/renderer/overlay/components/clock/Clock.tsx
+++ src/renderer/overlay/components/clock/Clock.tsx
@@ -1,6 +1,35 @@
 import { useCurrentTime } from "../../../hooks/useCurrentTime";
 
+const DayOfWeek = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+function fill2Digits(x: number) {
+  return x < 10 ? `0${x}` : `${x}`;
+}
+
 export function Clock() {
   const now = useCurrentTime();
-  return <div style={{ fontSize: "4vh", color: "white" }}>{now.toLocaleTimeString()}</div>;
+  const hour = fill2Digits(now.getHours());
+  const minute = fill2Digits(now.getMinutes());
+  const second = fill2Digits(now.getSeconds());
+
+  const month = now.getMonth();
+  const day = now.getDate();
+  const dayOfWeek = DayOfWeek[now.getDay()];
+
+  return (
+    <div className="clock-container">
+      <div className="clock font-oswald">
+        <div className="day">
+          {month}/{day} ({dayOfWeek})
+        </div>
+        <div className="time">
+          <p className="number">{hour}</p>
+          <p className="separator">:</p>
+          <p className="number">{minute}</p>
+          <p className="separator">:</p>
+          <p className="number">{second}</p>
+        </div>
+      </div>
+    </div>
+  );
 }
