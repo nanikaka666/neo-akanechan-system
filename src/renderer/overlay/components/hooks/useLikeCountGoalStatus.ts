dc4a325d3ed8980b8dc3c0bb1898f6389dc89427
nanikaka <nanikaka666@gmail.com>
2026-03-08 15:21:38 +0900

    implements useLikeCountGoalStatus hook

---
+++ src/renderer/overlay/components/hooks/useLikeCountGoalStatus.ts
@@ -0,0 +1,39 @@
+import { useCallback, useState } from "react";
+import { GoalsLevel, GoalsStatus } from "../../../../types/goals";
+import { useLiveSettings } from "./useLiveSettings";
+
+export function useLikeCountGoalStatus() {
+  const liveSettings = useLiveSettings();
+  const [likeCountGoalStatus, setLikeCountGoalStatus] = useState<GoalsStatus>({
+    type: "inProgress",
+    currentLevel: 1,
+  });
+
+  const likeCountPromotionFunc = useCallback(() => {
+    // Note: it doesn't work when describe logic with "prev" variable.
+    // likeCountPromotionFunc will be called multipe times at time (why?), so it causes unexpected over promotion.
+    // Work arround: describe promoted level with "likeCountGoalStatus".
+    setLikeCountGoalStatus(() => {
+      if (likeCountGoalStatus.type === "inProgress") {
+        if (likeCountGoalStatus.currentLevel === liveSettings.likeCountGoal.maxLevel) {
+          console.log("Like count Accomplished!");
+          return { type: "accomplished" };
+        } else {
+          console.log(
+            "Like count promotion!",
+            likeCountGoalStatus.currentLevel,
+            likeCountGoalStatus.currentLevel + 1,
+          );
+          return {
+            ...likeCountGoalStatus,
+            currentLevel: (likeCountGoalStatus.currentLevel + 1) as GoalsLevel,
+          };
+        }
+      } else {
+        return likeCountGoalStatus;
+      }
+    });
+  }, [likeCountGoalStatus, liveSettings]);
+
+  return [likeCountGoalStatus, likeCountPromotionFunc] as const;
+}
