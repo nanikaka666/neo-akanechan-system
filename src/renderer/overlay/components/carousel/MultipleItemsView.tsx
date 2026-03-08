f69c7eba90486315cbd24579e2fac986d3cf8c6e
nanikaka <nanikaka666@gmail.com>
2026-03-08 14:31:32 +0900

    refactor: don't use inline style

--- src/renderer/overlay/components/carousel/MultipleItemsView.tsx
+++ src/renderer/overlay/components/carousel/MultipleItemsView.tsx
@@ -9,14 +9,14 @@ export function MultipleItemsView({ items }: MultipleItemsViewProps) {
 
   return (
     <div
-      className="carousel-animation font-m-plus-rounded"
+      className="multiple carousel-animation font-m-plus-rounded"
       onAnimationIteration={() => {
         setPos((prev) => (prev + 1) % items.length);
       }}
     >
       {items.map((item, idx) => {
         return (
-          <div key={item.key} style={idx === pos % items.length ? {} : { display: "none" }}>
+          <div key={item.key} className={idx !== pos % items.length ? "not-selected" : ""}>
             {item}
           </div>
         );
