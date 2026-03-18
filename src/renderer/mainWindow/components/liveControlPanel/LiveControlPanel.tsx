0ea460026e5496402b29a8d4667c279536afa73d
nanikaka <nanikaka666@gmail.com>
2026-03-19 00:28:41 +0900

    replace with useMainContentsTab hook

--- src/renderer/mainWindow/components/liveControlPanel/LiveControlPanel.tsx
+++ src/renderer/mainWindow/components/liveControlPanel/LiveControlPanel.tsx
@@ -1,15 +1,14 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { SideBar } from "./SideBar";
 import { MainContents } from "./MainContents";
 import { useLiveLaunchProperties } from "../../../hooks/useLiveLaunchProperties";
 import { useLiveSettings } from "../../../hooks/useLiveSettings";
-
-export type Mode = "commentViewer" | "competition" | "rankings" | "goals";
+import { useMainContentsTab } from "../hooks/useMainContentsTab";
 
 export function LiveControlPanel() {
   const liveLaunchProperties = useLiveLaunchProperties();
   const liveSettings = useLiveSettings();
-  const [mode, setMode] = useState<Mode>("commentViewer");
+  const [currentMainContents, switchMainContents, allContentsNames] = useMainContentsTab();
 
   useEffect(() => {
     window.ipcApi.requestStartDataFetch().then(console.log);
@@ -17,8 +16,17 @@ export function LiveControlPanel() {
 
   return liveLaunchProperties && liveSettings ? (
     <>
-      <SideBar liveLaunchProperties={liveLaunchProperties} mode={mode} setMode={setMode} />
-      <MainContents liveSettings={liveSettings} mode={mode} />
+      <SideBar
+        liveLaunchProperties={liveLaunchProperties}
+        currentMainContents={currentMainContents}
+        switchMainContents={switchMainContents}
+        allContentsNames={allContentsNames}
+      />
+      <MainContents
+        liveSettings={liveSettings}
+        currentMainContents={currentMainContents}
+        allContentsNames={allContentsNames}
+      />
     </>
   ) : (
     <div>Now Loading</div>
