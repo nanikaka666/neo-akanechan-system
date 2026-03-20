d34fc5f9630b122e9c0ad645a9d61001d636b883
nanikaka <nanikaka666@gmail.com>
2026-03-21 07:06:49 +0900

    move ipc main handlers for user settings

--- src/main/ipc/handler/index.ts
+++ src/main/ipc/handler/index.ts
@@ -1,17 +1,10 @@
 import { IpcMainWrapper } from "../ipcMainWrapper";
-import { WebContentsWrapper } from "../../webContentsWrapper";
-import { UserSettingsService } from "../../userSettings";
 import {
   buildLiveLaunchProperties,
   buildLiveLaunchPropertiesForDebug,
 } from "../../liveLaunchProperties";
 import { VideoId } from "../../../types/youtubeDomainModel";
-import {
-  cleanupLiveManager,
-  getLiveManager,
-  isExistLiveManager,
-  setupLiveManager,
-} from "../../liveManager";
+import { cleanupLiveManager, getLiveManager, setupLiveManager } from "../../liveManager";
 import { getWindowManager } from "../../window";
 import { yesNoDialogOnMainWindow } from "../../dialog";
 import { getInitialMainAppPage, MainAppPageSwitcher } from "../../mainAppPage";
@@ -73,31 +66,6 @@ export function setupIpcMainHandlers() {
       }
       return false;
     }
-  });
-
-  IpcMainWrapper.handle("getUserSettings", () => {
-    return Promise.resolve(UserSettingsService.getUserSettings());
-  });
-
-  IpcMainWrapper.handle("saveUserSettings", (e, userSettings) => {
-    try {
-      UserSettingsService.setUserSettings(userSettings);
-      WebContentsWrapper.send(e.sender, "tellUpdatedUserSettings", userSettings);
-
-      // if LiveManager is up (it means user is in LiveStandBy page) then notify it to LiveManager.
-      if (isExistLiveManager()) {
-        getLiveManager().updateLiveSettings();
-      }
-
-      return Promise.resolve(true);
-    } catch (e: unknown) {
-      console.log(e);
-      return Promise.resolve(false);
-    }
-  });
-
-  IpcMainWrapper.handle("hasDifferenceAmongUserSettings", (e, settingsA, settingsB) => {
-    return Promise.resolve(!UserSettingsService.isEqual(settingsA, settingsB));
   });
 
   IpcMainWrapper.handle("startDataFetch", async () => {
--- src/main/ipc/handler/mainWindow/index.ts
+++ src/main/ipc/handler/mainWindow/index.ts
@@ -2,10 +2,12 @@ import { setupIpcMainHandlersForAuth } from "./auth";
 import { setupIpcMainHandlersForCommentViewer } from "./commentViewer";
 import { setupIpcMainHandlersForCompetition } from "./competition";
 import { setupIpcMainHandlersForRanking } from "./ranking";
+import { setupIpcMainHandlersForUserSettings } from "./userSettings";
 
 export function setupIpcMainHandlersForMainWindow() {
   setupIpcMainHandlersForAuth();
   setupIpcMainHandlersForCommentViewer();
   setupIpcMainHandlersForCompetition();
   setupIpcMainHandlersForRanking();
+  setupIpcMainHandlersForUserSettings();
 }
