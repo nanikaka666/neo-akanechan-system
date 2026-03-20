31c53d942aa3355f56cb6c02c20ba3c69e73f569
nanikaka <nanikaka666@gmail.com>
2026-03-21 07:02:25 +0900

    move ipc main handlers for competition

--- src/main/ipc/handler/index.ts
+++ src/main/ipc/handler/index.ts
@@ -144,41 +144,6 @@ export function setupIpcMainHandlers() {
     return Promise.resolve(true);
   });
 
-  IpcMainWrapper.handle("openCompetition", (_, question, options, acceptTimeMinutes) => {
-    getLiveManager().actionOpenCompetition(question, options, acceptTimeMinutes);
-    return Promise.resolve(true);
-  });
-
-  IpcMainWrapper.handle("abortCompetition", async () => {
-    const res = await yesNoDialogOnMainWindow(`本当にコンペを終了しますか？`);
-    if (!res) {
-      return false;
-    }
-    getLiveManager().actionAbortCompetition();
-    return true;
-  });
-
-  IpcMainWrapper.handle("answerDecision", async (_, answer, optionStr) => {
-    const res = await yesNoDialogOnMainWindow(
-      `コンペの正解を確定します`,
-      `${answer}: ${optionStr}`,
-    );
-    if (!res) {
-      return false;
-    }
-    getLiveManager().actionAnswerDecision(answer);
-    return true;
-  });
-
-  IpcMainWrapper.handle("manuallyEntryClose", async () => {
-    const res = await yesNoDialogOnMainWindow(`コンペの参加を締め切ります`);
-    if (!res) {
-      return false;
-    }
-    getLiveManager().actionManuallyEntryClose();
-    return true;
-  });
-
   IpcMainWrapper.handle("getLiveLaunchProperties", () => {
     return Promise.resolve(getLiveManager().getLiveLaunchProperties());
   });
--- src/main/ipc/handler/mainWindow/index.ts
+++ src/main/ipc/handler/mainWindow/index.ts
@@ -1,7 +1,9 @@
 import { setupIpcMainHandlersForAuth } from "./auth";
 import { setupIpcMainHandlersForCommentViewer } from "./commentViewer";
+import { setupIpcMainHandlersForCompetition } from "./competition";
 
 export function setupIpcMainHandlersForMainWindow() {
   setupIpcMainHandlersForAuth();
   setupIpcMainHandlersForCommentViewer();
+  setupIpcMainHandlersForCompetition();
 }
