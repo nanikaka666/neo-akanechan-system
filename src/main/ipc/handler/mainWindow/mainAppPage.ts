d4e2873b5a67883f984ef45dc013c4cd47859155
nanikaka <nanikaka666@gmail.com>
2026-03-23 14:01:29 +0900

    don't create overlay window when transit to live standby page

--- src/main/ipc/handler/mainWindow/mainAppPage.ts
+++ src/main/ipc/handler/mainWindow/mainAppPage.ts
@@ -10,46 +10,21 @@ import { VideoId } from "../../../../types/youtubeDomainModel";
 import { IpcMainWrapper } from "../../ipcMainWrapper";
 
 export function setupIpcMainHandlersForMainAppPage() {
-  IpcMainWrapper.handle("transitToLiveStandBy", async (e, channel, live) => {
-    if (getWindowManager().getOverlayWindow() !== undefined) {
-      // already overlay window opened.
-      return false;
-    }
-
-    const res = await yesNoDialogOnMainWindow(`ライブ配信を開始しますか？`, live.title);
-    if (!res) {
-      return false;
-    }
-
+  IpcMainWrapper.handle("transitToLiveStandBy", (e, channel, live) => {
     const liveLaunchProperties = buildLiveLaunchProperties(channel, live);
-
     setupLiveManager(liveLaunchProperties);
-    getWindowManager().createOverlayWindow(liveLaunchProperties.overlayWindowTitle, false);
 
     MainAppPageSwitcher.liveStandBy();
-    return true;
+    return Promise.resolve(true);
   });
 
   IpcMainWrapper.handle("transitToLiveStandByByVideoId", async (e, inputVideoId) => {
-    if (getWindowManager().getOverlayWindow() !== undefined) {
-      // already overlay window opened.
-      return false;
-    }
-
     try {
       const liveLaunchProperties = await buildLiveLaunchPropertiesForDebug(
         new VideoId(inputVideoId),
       );
-      const res = await yesNoDialogOnMainWindow(
-        `ライブ配信を開始しますか？`,
-        liveLaunchProperties.live.title,
-      );
-      if (!res) {
-        return false;
-      }
 
       setupLiveManager(liveLaunchProperties);
-      getWindowManager().createOverlayWindow(liveLaunchProperties.overlayWindowTitle, false);
 
       MainAppPageSwitcher.liveStandBy();
       return true;
