e7ddeb898773201b32f0a3bb2ff5b99c357ee8a7
nanikaka <nanikaka666@gmail.com>
2026-03-27 01:42:06 +0900

    styles lcp; wip

--- src/renderer/mainWindow/components/liveControlPanel/StatisticsArea.tsx
+++ src/renderer/mainWindow/components/liveControlPanel/StatisticsArea.tsx
@@ -5,29 +5,67 @@ export function StatisticsArea() {
 
   return (
     <div className="statistics">
-      <div>
-        高評価数(最大): {`${liveStatistics.currentLikeCount} (${liveStatistics.maxLikeCount})`}
-      </div>
-      <div>
-        同接数(最大): {`${liveStatistics.currentLiveViewCount}(${liveStatistics.maxLiveViewCount})`}
-      </div>
-      <div>テキストチャット: {liveStatistics.textChatCount}</div>
-      <div>SuperChat: {liveStatistics.superChatCount}</div>
-      <div>SuperSticker: {liveStatistics.superStickerCount}</div>
-      <div>チャットUU: {liveStatistics.chatUUCount}</div>
-      <div>
-        チャンネル登録者数(最大):{" "}
-        {`${liveStatistics.currentSubscriberCount}(${liveStatistics.maxSubscriberCount})`}
-      </div>
-      <div>新メンバーシップ: {liveStatistics.newMembershipsCount}</div>
-      <div>マイルストーン更新: {liveStatistics.membershipMilestoneCount}</div>
-      <div>ギフト回数: {liveStatistics.giftCount}</div>
-      <div>ギフト受取数: {liveStatistics.redemptionGiftCount}</div>
-      <div>ストック数: {liveStatistics.stocksCount}</div>
-      <div>
-        配信開始時間:{" "}
-        {liveStatistics.actualStartTime ? liveStatistics.actualStartTime.toLocaleString() : "---"}
-      </div>
+      <h2>統計情報</h2>
+      <ul>
+        <li>
+          <div>高評価数(最大):</div>
+          <div>{`${liveStatistics.currentLikeCount} (${liveStatistics.maxLikeCount})`}</div>
+        </li>
+        <li>
+          <div>同接数(最大):</div>
+          <div>{`${liveStatistics.currentLiveViewCount}(${liveStatistics.maxLiveViewCount})`}</div>
+        </li>
+        <li>
+          <div>テキストチャット:</div>
+          <div>{liveStatistics.textChatCount}</div>
+        </li>
+        <li>
+          <div>SuperChat:</div>
+          <div>{liveStatistics.superChatCount}</div>
+        </li>
+        <li>
+          <div>SuperSticker:</div>
+          <div>{liveStatistics.superStickerCount}</div>
+        </li>
+        <li>
+          <div>チャットUU:</div>
+          <div>{liveStatistics.chatUUCount}</div>
+        </li>
+        <li>
+          <div>チャンネル登録者数(最大):</div>
+          <div>
+            {`${liveStatistics.currentSubscriberCount}(${liveStatistics.maxSubscriberCount})`}
+          </div>
+        </li>
+        <li>
+          <div>新メンバーシップ:</div>
+          <div>{liveStatistics.newMembershipsCount}</div>
+        </li>
+        <li>
+          <div>マイルストーン更新:</div>
+          <div>{liveStatistics.membershipMilestoneCount}</div>
+        </li>
+        <li>
+          <div>ギフト回数:</div>
+          <div>{liveStatistics.giftCount}</div>
+        </li>
+        <li>
+          <div>ギフト受取数:</div>
+          <div>{liveStatistics.redemptionGiftCount}</div>
+        </li>
+        <li>
+          <div>ストック数:</div>
+          <div>{liveStatistics.stocksCount}</div>
+        </li>
+        <li>
+          <div>配信開始時間:</div>
+          <div>
+            {liveStatistics.actualStartTime
+              ? liveStatistics.actualStartTime.toLocaleString()
+              : "---"}
+          </div>
+        </li>
+      </ul>
     </div>
   );
 }
