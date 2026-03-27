76d3e59f138e16b0bd5418bd38e398a77d1f079f
nanikaka <nanikaka666@gmail.com>
2026-03-28 03:06:04 +0900

    chore: change order

--- src/renderer/mainWindow/hooks/useMainContentsTab.ts
+++ src/renderer/mainWindow/hooks/useMainContentsTab.ts
@@ -6,7 +6,7 @@ export function useMainContentsTab() {
   const [currentMainContents, setCurrentMainContents] = useState<MainContentsName>("goals");
   const switchMainContents = (to: MainContentsName) => setCurrentMainContents((_) => to);
 
-  const allContentsNames: MainContentsName[] = ["competition", "rankings", "goals"];
+  const allContentsNames: MainContentsName[] = ["goals", "rankings", "competition"];
 
   return [currentMainContents, switchMainContents, allContentsNames] as const;
 }
