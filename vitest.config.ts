f8462e065ceb1ea5b3e081c87ccad8868c10796e
nanikaka <nanikaka666@gmail.com>
2026-04-10 22:27:34 +0900

    use functions of vitest as globals

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,11 +2,13 @@ import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
+    globals: true,
     sequence: {
       shuffle: {
         files: true,
         tests: true,
       },
     },
+    include: ["**/test/**/*.test.{ts,tsx}"],
   },
 });
